78511f0c535a6cc05e50c3bddb2333ec49c2d67d
pick-cee <akinloluwaolumuyide@gmail.com>
2023-12-28 15:37:00 +0100

    added novu notification services

--- src/reminder/reminder.consumer.ts
+++ src/reminder/reminder.consumer.ts
@@ -57,7 +57,7 @@ export class ReminderConsumer {
                     email: user.email
                 },
                 payload: {
-                    firstName: user.firstName,
+                    firstname: user.firstName,
                     reminderDetails: reminder.details
                 }
             })
